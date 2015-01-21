ed9e3b023addcd5553f1e141abdd9a472ab936cf
polesye <s2pak.anton@gmail.com>
2015-01-21 17:18:53 +0200

    TNL-1233: Create configuration_models.

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -1518,6 +1518,7 @@ INSTALLED_APPS = (
     'licenses',
     'openedx.core.djangoapps.course_groups',
     'bulk_email',
+    'branding',
 
     # External auth (OpenID, shib)
     'external_auth',
